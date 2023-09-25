14e371c425f962ffc599bae76e037640503a7632
yates <yatesj3@mail.uc.edu>
2023-09-25 13:40:56 -0400

    Init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private String get_input() {
+        Scanner in = new Scanner(System.in);
+        return "";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
