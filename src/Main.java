4c3073eb5cae62929f769143b9ebeb78583f6b5a
yates <yatesj3@mail.uc.edu>
2023-09-25 15:03:17 -0400

    Program Logic

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,30 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-
-    private String get_input() {
+    private static int get_int() {
+        System.out.print("Please enter an integer price: ");
         Scanner in = new Scanner(System.in);
-        return "";
+        int res;
+        try {
+            res = in.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+            System.out.println("Input an integer please.");
+            return get_int();
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int price = get_int();
+        double shipping_fee;
+        if (price >= 100) {
+            shipping_fee = 0;
+        } else {
+            shipping_fee = price * 0.02;
+        }
+        double total = price + shipping_fee;
+        System.out.print("Shipping: $" + shipping_fee + "; Total: $" + total);
     }
 }
